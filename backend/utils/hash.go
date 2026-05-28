c4549b6b233345c2f869936ddd382a9c8a64f352
xbuyan <kevobuyan@gmail.com>
2026-05-28 15:11:28 +0300

    Feat: Added hash.go, jwt.go, middleware.go

---
+++ backend/utils/hash.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+// GenerateHash simulates creating an immutable on-chain hash for medical records
+func GenerateHash(data string) string {
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
+}
