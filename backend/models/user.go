8a27753014027d17eb8420856d807f6f48d1f055
okelloodhiambocvs <okelloodhiambocvs@gmail.com>
2026-05-28 15:12:30 +0300

    feat(models): improvised user model structure

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,9 +1,13 @@
 package models
 
 type User struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Role     string `json:"role"`
+	ID          int    `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	Role        string `json:"role"`
+	ProfilePic  string `json:"profile_pic"`
+	Gender      string `json:"gender"`
+	YearOfBirth int    `json:"year_of_birth"`
 }
